dd7defb64e8f8a66943dceb0792737c98d00e3d8
platonfrolov <pl.frolov99@gmail.com>
2023-08-01 01:27:35 +0200

    fix browserrouter issue

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 // root.render(<App />);
 root.render((
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
    
       {/* <Route path="/" element={<AllMeetupsPage />} /> */}
